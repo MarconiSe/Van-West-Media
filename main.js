71bd6a01018a00660a60743e1d7bfabac0c88a1d
MarconiSe <hnrygmz@gmail.com>
2024-09-10 21:50:34 -0600

    new changes on filter dropdown

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 document.getElementById('dropdownButton').addEventListener('click', function () {
   const menu = document.getElementById('dropdownMenu');
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-});
-
-// Close dropdown when clicking outside
-window.addEventListener('click', function (e) {
-  const button = document.getElementById('dropdownButton');
-  const menu = document.getElementById('dropdownMenu');
-  if (!button.contains(e.target) && !menu.contains(e.target)) {
-      menu.style.display = 'none';
-  }
+  menu.classList.toggle('hidden'); // Show or hide the menu
 });
