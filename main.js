09fda9a4538ea1c072b8ae4a457ab46c54af21d7
MarconiSe <hnrygmz@gmail.com>
2024-09-16 21:35:37 -0600

    turned work section cards into a JS template

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+const template = document.querySelector("#work-card-template");
+const wrapper = document.createDocumentFragment();
+
 document.getElementById('dropdownButton').addEventListener('click', function () {
   const menu = document.getElementById('dropdownMenu');
   menu.classList.toggle('hidden'); // Show or hide the menu
 });
 
 async function workArea() {
-  const workPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
-  const workData = await workPromise.json()
+  const workPromise = await fetch("https://sunny-sfogliatella-40c6d1.netlify.app/.netlify/functions/work");
+  const workData = await workPromise.json();
   workData.forEach(work => {
-    console.log{work.species}
-  })
+      const clone = template.content.cloneNode(true);
+
+      clone.querySelector(".card-title").textContent = work.Name;
+
+      wrapper.appendChild(clone); // Append the clone to the wrapper
+  });
+  document.querySelector(".work-section").appendChild(wrapper); // Append the wrapper to the work-section
 }
 
-workArea()
+workArea();
