524f3fd84bf25151098edc02a355c5a156b86240
MarconiSe <hnrygmz@gmail.com>
2024-09-16 20:13:12 -0600

    work in progressm fetched a json

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ document.getElementById('dropdownButton').addEventListener('click', function () 
   const menu = document.getElementById('dropdownMenu');
   menu.classList.toggle('hidden'); // Show or hide the menu
 });
+
+async function workArea() {
+  const workPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const workData = await workPromise.json()
+  workData.forEach(work => {
+    console.log{work.species}
+  })
+}
+
+workArea()
